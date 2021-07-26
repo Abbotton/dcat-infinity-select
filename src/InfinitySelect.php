f18ae1c228c401e15fa12c5f14a9046bebef97a0
Abbotton <uctoo@foxmail.com>
2021-07-26 15:46:12 +0800

    :sparkles: 添加遗漏注释

--- src/InfinitySelect.php
+++ src/InfinitySelect.php
@@ -8,6 +8,12 @@ class InfinitySelect extends Select
 {
     protected $view = 'dcat-infinity-select::index';
 
+    /**
+     * 设定有序链表的值.
+     *
+     * @param  string  $list
+     * @return $this
+     */
     public function list(string $list)
     {
         $this->addVariables(['valueList' => $list]);
